659d1ebdc064f853573ac3b31e352e184edfd33b
lana fox <lana@gmail.com>
2023-07-21 15:45:37 +0400

    tablet

---
+++ js.js
@@ -0,0 +1,17 @@
+const lower = 'abcdefghijklmnopqrstuvwxyz';
+const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const digits = '0123456789';
+const symbols = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+
+const password_display = document.querySelector(".password-display")
+const copy = document.querySelector(".copy")
+const copy_img = document.querySelector("#copy-img")
+const lngt = document.querySelector("#lngt")
+const slider = document.querySelector(".slider")
+const checkbox = document.querySelector(".checkbox")
+const strong_change = document.querySelector(".strong-change")
+const one = document.querySelector("#one")
+const two = document.querySelector("#two")
+const three = document.querySelector("#three")
+const four = document.querySelector("#four")
+
