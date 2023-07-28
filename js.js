85a3bcfa88682c841adf51d7e3c728510bebfb6c
lana fox <lana@gmail.com>
2023-07-28 16:02:01 +0400

    js.js

--- js.js
+++ js.js
@@ -1,17 +1,65 @@
-const lower = 'abcdefghijklmnopqrstuvwxyz';
-const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const digits = '0123456789';
-const symbols = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const lower = 'abcdefghijklmnopqrstuvwxyz'
+const numbers = '0123456789'
+const symbols = '!@#$%^&*()_+~`|}{[]\:;?><,./-='
 
 const password_display = document.querySelector(".password-display")
 const copy = document.querySelector(".copy")
 const copy_img = document.querySelector("#copy-img")
 const lngt = document.querySelector("#lngt")
 const slider = document.querySelector(".slider")
-const checkbox = document.querySelector(".checkbox")
-const strong_change = document.querySelector(".strong-change")
-const one = document.querySelector("#one")
-const two = document.querySelector("#two")
-const three = document.querySelector("#three")
-const four = document.querySelector("#four")
+
+const checkboxes = document.querySelectorAll(".checkbox");
+const cols = document.querySelectorAll('.col');
+const strongChange = document.querySelectorAll('.strong-change');
+const colors = ['#F64A4A', '#FB7C58', '#F8CD65', '#A4FFAF']
+
+function strength() {
+  const checkedLength = document.querySelectorAll('.checkbox:checked').length;
+  const chosenColor = colors[checkedLength - 1];
+  
+  cols.forEach(col => {
+    col.style.backgroundColor = 'transparent'
+    col.style.border = '1px solid #E6E5EA'
+  });
+  strongChange.forEach(st => st.style.display = 'none');
+
+
+  for (let index = 0; index < checkedLength; index++) {
+    const col = cols[index]
+    col.style.backgroundColor = chosenColor
+  }
+
+  if (checkedLength > 0) strongChange[checkedLength - 1].style.display = 'block';
+}
+
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', strength))
+
+copy_img.addEventListener("click", () => {
+    const suggestedPassword = password_display.textContent
+    const textarea = document.createElement("textarea")
+    textarea.value = suggestedPassword
+    document.body.appendChild(textarea)
+
+    textarea.select()
+    document.execCommand("copy")
+
+    document.body.removeChild(textarea)
+    copy.style.display = "block"
+
+    setTimeout(() => {
+        copy.style.display = "none"
+    }, 2000)
+})
+
+function updateSliderColor() {
+  const percent = (slider.value - slider.min) / (slider.max - slider.min);
+  const color = `linear-gradient(to right, #A4FFAF ${percent * 100}%, #fff ${percent * 100}%)`;
+  slider.style.background = color;
+  lngt.textContent = slider.value;
+}
+
+slider.addEventListener("input", () => {
+    updateSliderColor();
+});
 
